e894d5ffcceb94ae96de7922e58cd34d814c2b5a
Nicolas20578 <niko20578@gmail.com>
2023-03-01 21:04:47 -0600

    Mi Tercer commit

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 import express from 'express'
 import usuarioRoutes from './routes/usuarioRoutes.js'
+import db from './config/db.js'
 
 // Crear la app
 const app = express()
+
+// Habilitar lectura de datos de formularios
+app.use( express.urlencoded({extended: true}) )
+
+// Conexion a la base de datos
+try {
+    await db.authenticate();
+    db.sync()
+    console.log('Conexión Correcta a la Base de Datos')
+} catch (error) {
+    console.log(error)
+}
 
 // Habilitar Pug
 app.set('view engine', 'pug')
